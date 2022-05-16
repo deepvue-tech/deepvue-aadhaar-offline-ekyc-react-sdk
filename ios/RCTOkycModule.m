3fa3b55b15de579710d8a85b4bd15764fb70d890
Chandrashekhar <chandrashekhar1503@gmail.com>
2022-05-16 22:32:50 +0530

    add facematch param

--- ios/RCTOkycModule.m
+++ ios/RCTOkycModule.m
@@ -7,15 +7,18 @@
 
 RCT_EXPORT_MODULE(OkycHandler);
 
-RCT_EXPORT_METHOD(initSdk:(NSString *)clientId clientSecret:(NSString *)clientSecret baseUrl:(NSString *)baseUrl imageUrl:(NSString *)imageUrl success:(RCTResponseSenderBlock)success failure:(RCTResponseSenderBlock)failure )
+RCT_EXPORT_METHOD(initSdk:(NSString *)clientId clientSecret:(NSString *)clientSecret useFaceMatch:(BOOL *)useFaceMatch baseUrl:(NSString *)baseUrl imageUrl:(NSString *)imageUrl success:(RCTResponseSenderBlock)success failure:(RCTResponseSenderBlock)failure )
 {
+    
   dispatch_async(dispatch_get_main_queue(), ^{
     NSString * imagePath = nil;
     if([imageUrl length]>0){
       imagePath = imageUrl;
     }
     OkycSdkHandler * okycHandler = [[OkycSdkHandler alloc] init];
-    [okycHandler init:baseUrl clientId:clientId clientSecret:clientSecret useFaceMatch:NO successBlock:^(NSString *response) {
+    [okycHandler init:baseUrl clientId:clientId clientSecret:clientSecret useFaceMatch:useFaceMatch successBlock:^(NSString *response) {
+      
+
       success([NSArray arrayWithObjects: response, nil]);
     } failureBlock:^(NSInteger code) {
       failure([NSArray arrayWithObjects: [NSNumber numberWithInteger:code], nil]);
